a8fd22dd38420a285da018b0c505d8fb24017806
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-07-19 15:50:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,11 +17,11 @@ function App() {
         <Hero />
         <BorderSec />
         <AboutUs />
-        <div className=" relative flex flex-col sm:flex-row justify-evenly items-center gap-8 py-[2%] bg-[#0f131c] "  data-aos="zoom-in-up"
-          data-aos-duration="1500">
+        <div className=" relative flex flex-col sm:flex-row justify-evenly items-center gap-8 py-[2%] bg-[#0f131c] "  >
         
 
-          <h1 className=" text-3xl sm:text-6xl font-bold text-white font-dream">
+          <h1 className=" text-3xl sm:text-6xl font-bold text-white font-dream" data-aos="zoom-in-up"
+          data-aos-duration="1500">
             Tokenomics
           </h1>
 
