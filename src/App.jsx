115292c3126638ea0088f5d335a898a814233088
noor alam <51040978+noorshanu@users.noreply.github.com>
2024-07-19 15:48:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import AboutUs from "./components/AboutUs";
 import BorderSec from "./components/BorderSec";
 import ChooseHero from "./components/ChooseHero";
-import DexMark from "./components/dexMark";
+import DexMark from "./components/DexMark";
 import Faq from "./components/Faq";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
@@ -17,7 +17,8 @@ function App() {
         <Hero />
         <BorderSec />
         <AboutUs />
-        <div className=" relative flex flex-col sm:flex-row justify-evenly items-center gap-8 py-[2%] bg-[#0f131c] ">
+        <div className=" relative flex flex-col sm:flex-row justify-evenly items-center gap-8 py-[2%] bg-[#0f131c] "  data-aos="zoom-in-up"
+          data-aos-duration="1500">
         
 
           <h1 className=" text-3xl sm:text-6xl font-bold text-white font-dream">
